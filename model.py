71c4852603b6a06956538858fb973475d0b1ea26
Martin RS <maros105@gmail.com>
2019-08-21 08:57:21 +0200

    Model reduceret til tomme klasser

--- model.py
+++ model.py
@@ -1,73 +1,16 @@
 from random import randint
 
 class Car(object):
-    def __init__(self):
-        self.theEngine = Engine()
-
-    def updateModel(self, dt):
-        self.theEngine.updateModel(dt)
+    pass
 
 class Wheel(object):
-    def __init__(self):
-        self.orientation = randint(0, 360)
-
-    def rotate(self, revolutions):
-        self.orientation = (self.orientation + (revolutions * 360)) % 360
+    pass
 
 class Engine(object):
-    def __init__(self):
-        self.theGearbox = Gearbox()
-        self.theTank = Tank()
-
-        self.maxRpm = 100
-        self.currentRpm = 0
-        self.throttlePosition = 0
-        self.consumptionConstant = 0.0025
-
-    def updateModel(self, dt):
-        if self.theTank.contents > 0:
-            self.currentRpm = self.maxRpm * self.throttlePosition
-            self.theTank.remove(self.currentRpm * self.consumptionConstant)
-            self.theGearbox.rotate(self.currentRpm * (dt / 60))
+    pass
 
 class Gearbox(object):
-    """
-    frontLeft = Wheel()
-    frontRight = Wheel()
-    rearLeft = Wheel()
-    rearRight = Wheel()"""
-
-    def __init__(self):
-        self.clutchEngaged = False
-        self.gears = [0, 0.8, 1, 1.4, 2.2, 3.8]
-        self.currentGear = 0
-        self.wheels = {}
-        for wheel in ['frontLeft', 'frontRight', 'rearLeft', 'rearRight']:
-            self.wheels[wheel] = Wheel()
-
-    def shiftUp(self):
-        if not self.currentGear == len(self.gears) and not self.clutchEngaged:
-            self.currentGear = self.currentGear + 1
-
-    def shiftDown(self):
-        if self.currentGear > 0 and not self.clutchEngaged:
-            self.currentGear = self.currentGear - 1
-
-    def rotate(self, revolutions):
-        if self.clutchEngaged:
-            for wheel in self.wheels:
-                self.wheels[wheel].rotate(revolutions * self.gears[self.currentGear])
+    pass
 
 class Tank(object):
-    def __init__(self):
-        self.capacity = 100
-        self.contents = 100
-
-    def remove(self, amount):
-        if self.contents > amount:
-            self.contents = self.contents - amount
-        else:
-            self.contents = 0
-
-    def refuel(self):
-        self.contents = self.capacity
+    pass
