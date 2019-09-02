7a304c456e15d8a10157382df61b4b817d123b44
Martin RS <maros105@gmail.com>
2019-09-02 10:30:41 +0200

    I gang med Gearbox

--- model.py
+++ model.py
@@ -4,13 +4,39 @@ class Car(object):
     pass
 
 class Wheel(object):
-    pass
+
+    def __init__(self):
+        self.orientation = randint(0, 360)
+
+    def rotate(self, revolutions):
+        self.orientation = (self.orientation + (revolutions * 360)) % 360
 
 class Engine(object):
     pass
 
 class Gearbox(object):
-    pass
+
+    def __init__(self):
+        self.wheels = {'frontLeft': Wheel(), 'frontRight': Wheel(), 'rearLeft': Wheel(), 'rearRight': Wheel()}
+        self.gears = [0, 0.8, 1, 1.4, 2.2, 3.8]
+        self.clutchEngaged = False
+        self.currentGear = 0
+
+    # Vi glemte koblingen!
+    def shiftUp(self):
+        if self.currentGear < len(self.gears)-1:
+            self.currentGear = self.currentGear + 1
 
 class Tank(object):
-    pass
+
+    def __init__(self):
+        self.capacity = 100
+        self.contents = 100
+
+    def refuel(self):
+        self.contents = self.capacity
+
+    def remove(self, amount):
+        self.contents = self.contents - amount
+        if self.contents < 0:
+            self.contents = 0
